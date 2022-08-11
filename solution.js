9267db46b09eef8d6003361fa84348305df07a58
Ben Berry <ben.berry@developersinstitute.co.nz>
2022-08-12 10:26:59 +1200

    refactor: add switch statement option

--- solution.js
+++ solution.js
@@ -1,20 +1,38 @@
 const pairElement = (str) => {
-  const splitStr = str.split("");
+  if (!str || typeof str !== "string") {
+    throw new Error("Please provide a valid string");
+  }
 
   const dnaStrand = [];
 
-  for (const letter of splitStr) {
-    if (letter === "A") {
-      dnaStrand.push(["A", "T"]);
-    }
-    if (letter === "T") {
-      dnaStrand.push(["T", "A"]);
-    }
-    if (letter === "C") {
-      dnaStrand.push(["C", "G"]);
-    }
-    if (letter === "G") {
-      dnaStrand.push(["G", "C"]);
+  for (const letter of str) {
+    // if (letter === "A") {
+    //   dnaStrand.push(["A", "T"]);
+    // }
+    // if (letter === "T") {
+    //   dnaStrand.push(["T", "A"]);
+    // }
+    // if (letter === "C") {
+    //   dnaStrand.push(["C", "G"]);
+    // }
+    // if (letter === "G") {
+    //   dnaStrand.push(["G", "C"]);
+    // }
+
+    // Switch alternative
+    switch (letter) {
+      case "A":
+        dnaStrand.push(["A", "T"]);
+        break;
+      case "T":
+        dnaStrand.push(["T", "A"]);
+        break;
+      case "C":
+        dnaStrand.push(["C", "G"]);
+        break;
+      case "G":
+        dnaStrand.push(["G", "C"]);
+        break;
     }
   }
 
